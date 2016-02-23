5fbd19ba2f5cb2e2d20192543b22013725ac43a1
Christian Oelsner <christian-oelsner@hotmail.com>
2016-02-23 09:33:26 +0100

    Opdateret

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by oelsner on 23/02/16.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Customer customer = new Customer("Christian", "Oelsner", "12345678", 40);
+
+
+
+    }
+}
