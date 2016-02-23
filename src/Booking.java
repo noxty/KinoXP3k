3aefc49d765422945444b73f07b2819f5e38fcb1
Christian Oelsner <christian-oelsner@hotmail.com>
2016-02-23 09:22:55 +0100

    Opdateret

--- src/Booking.java
+++ src/Booking.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oelsner on 22/02/16.
@@ -7,4 +9,5 @@ import java.util.List;
 public class Booking
 {
     private List<Customer> customerList = new ArrayList<>();
+
 }
--- src/Movie.java
+++ src/Movie.java
@@ -3,4 +3,32 @@
  */
 public class Movie
 {
+    private String title;
+    private String rating;
+
+    public Movie(String title, String rating)
+    {
+        this.title = title;
+        this.rating = rating;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getRating()
+    {
+        return rating;
+    }
+
+    public void setRating(String rating)
+    {
+        this.rating = rating;
+    }
 }
