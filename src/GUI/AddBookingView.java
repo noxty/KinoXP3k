53f67e4fd48c60d5222f3d5713798b682b5b5f64
Hans <imorgenerdetmandag@gmail.com>
2016-02-24 14:26:48 +0100

    1 iteration, done.

--- src/GUI/AddBookingView.java
+++ src/GUI/AddBookingView.java
@@ -19,21 +19,8 @@ public class AddBookingView
         layoutGrid.setHgap(gaps);
         layoutGrid.setVgap(gaps);
 
-        // Search
-        Label labelSearchTitle = new Label("Search");
-        Label labelSearchYear = new Label("Year");
-
-        TextField fieldSearchTitle = new TextField();
-        TextField fieldSearchYear = new TextField();
-
-        Button buttonSearch = new Button("Search");
-
-        HBox searchBox = new HBox(gaps);
-        searchBox.setAlignment(Pos.CENTER_LEFT);
-        searchBox.getChildren().addAll(fieldSearchTitle, labelSearchYear, fieldSearchYear, buttonSearch);
-
         // Buttons
-        Button buttonAdd = new Button("Add Movie");
+        Button buttonAdd = new Button("Add Booking");
         Button buttonClear = new Button("Clear All");
 
         HBox buttonBox = new HBox(gaps);
@@ -44,52 +31,26 @@ public class AddBookingView
         Label labelDate = new Label("Date");
         Label labelSeats = new Label("Seats");
         Label labelTheater = new Label("Theater");
+        Label labelPickSeats = new Label("Valgte Sæder");
 
-        ComboBox<String> comboSeats = new ComboBox<>();
+        ComboBox<Integer> comboSeats = new ComboBox<>();
+        comboSeats.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8);
+
+        ComboBox<String> comboTheater = new ComboBox<>();
+        comboTheater.getItems().addAll("Den Store Sal", "Den Lille Sal");
+
+        TextField fieldPickedSeats = new TextField("Placeholder for valgte sæder");
 
 
         layoutGrid.add(labelDate, 0, 0);
         layoutGrid.add(DateBox.getDateBox(), 1, 0);
         layoutGrid.add(labelSeats, 0, 1);
         layoutGrid.add(comboSeats, 1, 1);
-
-        // Standard Formula
-        /*
-        Label labelTitle = new Label("Title");
-        Label labelDescription = new Label("Description");
-        Label labelAgeRestriction = new Label("Age Restriction");
-        Label labelPlaytime = new Label("Playtime");
-        Label labelPremiere = new Label("Premiere");
-        Label labelPrice = new Label("Price");
-        Label labelGenre = new Label("Genre");
-
-        TextField fieldTitle = new TextField();
-        TextArea fieldDescription = new TextArea();
-        TextField fieldAgeRestriction = new TextField();
-        TextField fieldPlaytime = new TextField();
-        TextField fieldPremiere = new TextField();
-        TextField fieldPrice = new TextField();
-        TextField fieldGenre = new TextField();
-
-        layoutGrid.add(labelSearchTitle, 0, 0);
-        layoutGrid.add(searchBox, 1, 0);
-        layoutGrid.add(labelTitle, 0, 1);
-        layoutGrid.add(fieldTitle, 1, 1);
-        layoutGrid.add(labelDescription, 0, 2);
-        layoutGrid.add(fieldDescription, 1, 2);
-        layoutGrid.add(labelGenre, 0, 3);
-        layoutGrid.add(fieldGenre, 1, 3);
-        layoutGrid.add(labelAgeRestriction, 0, 4);
-        layoutGrid.add(fieldAgeRestriction, 1, 4);
-        layoutGrid.add(labelPlaytime, 0, 5);
-        layoutGrid.add(fieldPlaytime, 1, 5);
-        layoutGrid.add(labelPremiere, 0, 6);
-        layoutGrid.add(fieldPremiere, 1, 6);
-        layoutGrid.add(labelPrice, 0, 7);
-        layoutGrid.add(fieldPrice, 1, 7);
-        layoutGrid.add(buttonBox, 1, 8);
-        */
-
+        layoutGrid.add(labelTheater, 0, 2);
+        layoutGrid.add(comboTheater, 1, 2);
+        layoutGrid.add(labelPickSeats, 0, 3);
+        layoutGrid.add(fieldPickedSeats, 1, 3);
+        layoutGrid.add(buttonBox, 1, 4);
 
         layout.getChildren().add(layoutGrid);
         return layout;
