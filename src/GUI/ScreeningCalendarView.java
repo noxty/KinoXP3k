bd12265e12b559edbb1f9ef311dedd6e253a685d
Hans <imorgenerdetmandag@gmail.com>
2016-02-26 09:05:05 +0100

    har ændret på nogle controllers

---
+++ src/GUI/ScreeningCalendarView.java
@@ -0,0 +1,40 @@
+package GUI;
+
+import Controller.TimeController;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.Calendar;
+
+/**
+ * Created by Hans on 25-02-2016.
+ */
+public class ScreeningCalendarView
+{
+    public static Double dayWidth = 150.0;
+    public static Double dayHeight = 600.0;
+    public static int showDays = 7;
+
+    public static HBox getView()
+    {
+        HBox layout = new HBox(10);
+
+        int calDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int calMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int calYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        String dateToString;
+
+        long today = TimeController.getScreeningDay(134);
+
+        for (int i = 0; i < showDays+1; i++)
+        {
+            VBox day = new VBox();
+
+
+        }
+
+
+        return layout;
+    }
+}
